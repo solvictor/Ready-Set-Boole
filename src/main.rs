f02cc4a83b52b9c225deaccc8559623bed2675e8
solvictor <victor.egret.pro@gmail.com>
2024-12-14 23:20:19 +0100

    multiplier

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 mod operators;
 
-use operators::adder;
+use operators::multiplier;
 
-fn main() {}
+fn main() {
+    let a: u32 = u32::MAX;
+    let b: u32 = u32::MAX;
+    println!("a = {}, b = {}, a * b = {}", a, b, multiplier(a, b));
+}
--- src/operators.rs
+++ src/operators.rs
@@ -7,6 +7,18 @@ pub fn adder(mut a: u32, mut b: u32) -> u32 {
     a
 }
 
+pub fn multiplier(mut a: u32, mut b: u32) -> u32 {
+    let mut res = 0;
+    while b > 0 {
+        if b & 1 != 0 {
+            res = adder(res, a);
+        }
+        a <<= 1;
+        b >>= 1;
+    }
+    res
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -15,15 +27,33 @@ mod tests {
     fn test_add() {
         assert_eq!(adder(0, 0), 0);
         assert_eq!(adder(0, 9), 9);
-        assert_eq!(adder(53, 87), 140);
-        assert_eq!(adder(47, 18), 65);
-        assert_eq!(adder(18, 47), 65);
-        assert_eq!(adder(62, 80), 142);
+        assert_eq!(adder(53, 87), 53 + 87);
+        assert_eq!(adder(47, 18), 47 + 18);
+        assert_eq!(adder(18, 47), 18 + 47);
+        assert_eq!(adder(62, 80), 62 + 80);
     }
 
     #[test]
     fn test_add_overflows() {
         assert_eq!(adder(u32::MAX, 1), 0);
         assert_eq!(adder(u32::MAX, u32::MAX), u32::MAX - 1);
+    }
+
+    #[test]
+    fn test_mul() {
+        assert_eq!(multiplier(0, 0), 0);
+        assert_eq!(multiplier(0, 9), 0);
+        assert_eq!(multiplier(1, 1), 1);
+        assert_eq!(multiplier(53, 87), 53 * 87);
+        assert_eq!(multiplier(47, 18), 47 * 18);
+        assert_eq!(multiplier(18, 47), 18 * 47);
+        assert_eq!(multiplier(62, 80), 62 * 80);
+    }
+
+    #[test]
+    #[allow(arithmetic_overflow)]
+    fn test_mul_overflows() {
+        assert_eq!(multiplier(u32::MAX, 1), u32::MAX);
+        assert_eq!(multiplier(u32::MAX, u32::MAX), u32::MAX * u32::MAX);
     }
 }
